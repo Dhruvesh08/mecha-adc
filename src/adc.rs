a0efd887a835ca852702f9ca912a82fb0af9a611
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-11 18:35:08 +0530

    mecha-adc-with-updated-freq

--- src/adc.rs
+++ src/adc.rs
@@ -1,18 +1,21 @@
-use std::fs::File;
-use std::io::{Read, Result};
+use std::fs::{File, OpenOptions};
+use std::io::{Read, Seek, SeekFrom, Write, Result};
 
 pub struct Adc {
     voltage0_file: File,
     voltage1_file: File,
+    sampling_frequency_file: File,
 }
 
 impl Adc {
     pub fn new() -> Result<Self> {
         let voltage0_file = File::open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw")?;
         let voltage1_file = File::open("/sys/bus/iio/devices/iio:device0/in_voltage1_raw")?;
+        let sampling_frequency_file = File::open("/sys/bus/iio/devices/iio:device0/in_voltage0_sampling_frequency")?;
         Ok(Self {
             voltage0_file,
             voltage1_file,
+            sampling_frequency_file,
         })
     }
 
@@ -26,5 +29,20 @@ impl Adc {
         let mut buffer = String::new();
         self.voltage1_file.read_to_string(&mut buffer)?;
         Ok(buffer.trim().parse::<u16>().unwrap())
+    }
+
+    pub fn read_sampling_frequency(&mut self) -> Result<u32> {
+        let mut buffer = String::new();
+        self.sampling_frequency_file.seek(SeekFrom::Start(0))?;
+        self.sampling_frequency_file.read_to_string(&mut buffer)?;
+        Ok(buffer.trim().parse::<u32>().unwrap())
+    }
+
+    pub fn set_sampling_frequency(&mut self, frequency: u32) -> Result<()> {
+        let mut file = OpenOptions::new()
+            .write(true)
+            .open("/sys/bus/iio/devices/iio:device0/in_voltage0_sampling_frequency")?;
+        file.write_all(frequency.to_string().as_bytes())?;
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,10 @@ fn main() -> Result<()> {
     let voltage1 = adc.read_voltage1()?;
     println!("Voltage 0: {}", voltage0);
     println!("Voltage 1: {}", voltage1);
+    let sampling_frequency = adc.read_sampling_frequency()?;
+    println!("Sampling Frequency: {}", sampling_frequency);
+    adc.set_sampling_frequency(1000)?;
+    println!("Sampling frequency set to 1000");
+    println!("updated Sampling Frequency: {}", adc.read_sampling_frequency()?);
     Ok(())
 }
