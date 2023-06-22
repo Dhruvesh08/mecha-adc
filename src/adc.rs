324a81e0653010b96b11b432d2b9778bdad87be9
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-22 14:27:03 +0530

    adc new design added

--- src/adc.rs
+++ src/adc.rs
@@ -1,77 +1,40 @@
-use std::fs::{File, OpenOptions};
-use std::io::{Read, Seek, SeekFrom, Write, Result};
+use std::fs::File;
+use std::io::{Read, Result};
 
-#[allow(dead_code)]
-pub enum SamplingFrequency {
-    F8,
-    F16,
-    F32,
-    F64,
-    F128,
-    F250,
-    F475,
-    F860,
+pub trait AdcDevice<'a> {
+    fn set_device(&mut self, channel_1_path: &'a str, channel_2_path: &'a str) -> Result<()>;
+    fn get_device(&self) -> Option<(&str, &str)>;
+    fn read_channel1(&mut self) -> Result<u16>;
+    fn read_channel2(&mut self) -> Result<u16>;
 }
 
-impl SamplingFrequency {
-    fn to_string(&self) -> String {
-        match self {
-            SamplingFrequency::F8 => "8".to_string(),
-            SamplingFrequency::F16 => "16".to_string(),
-            SamplingFrequency::F32 => "32".to_string(),
-            SamplingFrequency::F64 => "64".to_string(),
-            SamplingFrequency::F128 => "128".to_string(),
-            SamplingFrequency::F250 => "250".to_string(),
-            SamplingFrequency::F475 => "475".to_string(),
-            SamplingFrequency::F860 => "860".to_string(),
-        }
-    }
+pub struct Adc<'a> {
+  pub  channel_1_path: &'a str,
+  pub  channel_2_path: &'a str,
 }
 
-pub struct Adc {
-    voltage0_file: File,
-    voltage1_file: File,
-    sampling_frequency_file: File,
-}
-
-impl Adc {
-    pub fn new() -> Result<Self> {
-        let voltage0_file = File::open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw")?;
-        let voltage1_file = File::open("/sys/bus/iio/devices/iio:device0/in_voltage1_raw")?;
-        let sampling_frequency_file =
-            File::open("/sys/bus/iio/devices/iio:device0/in_voltage0_sampling_frequency")?;
-        Ok(Self {
-            voltage0_file,
-            voltage1_file,
-            sampling_frequency_file,
-        })
-    }
-
-    pub fn read_voltage0(&mut self) -> Result<u32> {
-        let mut buffer = String::new();
-        self.voltage0_file.read_to_string(&mut buffer)?;
-        print!("{}", buffer);
-        Ok(buffer.trim().parse::<u32>().unwrap())
-    }
-
-    pub fn read_voltage1(&mut self) -> Result<u32> {
-        let mut buffer = String::new();
-        self.voltage1_file.read_to_string(&mut buffer)?;
-        Ok(buffer.trim().parse::<u32>().unwrap())
-    }
-
-    pub fn read_sampling_frequency(&mut self) -> Result<u32> {
-        let mut buffer = String::new();
-        self.sampling_frequency_file.seek(SeekFrom::Start(0))?;
-        self.sampling_frequency_file.read_to_string(&mut buffer)?;
-        Ok(buffer.trim().parse::<u32>().unwrap())
-    }
-
-    pub fn set_sampling_frequency(&mut self, frequency: SamplingFrequency) -> Result<()> {
-        let mut file = OpenOptions::new()
-            .write(true)
-            .open("/sys/bus/iio/devices/iio:device0/in_voltage0_sampling_frequency")?;
-        file.write_all(frequency.to_string().as_bytes())?;
+impl<'a> AdcDevice<'a> for Adc<'a> {
+    fn set_device(&mut self, channel_1_path: &'a str, channel_2_path: &'a str) -> Result<()> {
+        self.channel_1_path = channel_1_path;
+        self.channel_2_path = channel_2_path;
         Ok(())
     }
+
+    fn get_device(&self) -> Option<(&str, &str)> {
+        Some((self.channel_1_path, self.channel_2_path))
+    }
+
+    fn read_channel1(&mut self) -> Result<u16> {
+        let mut buffer = String::new();
+        File::open(self.channel_1_path)?.read_to_string(&mut buffer)?;
+        Ok(buffer.trim().parse::<u16>().unwrap())
+    }
+
+    fn read_channel2(&mut self) -> Result<u16> {
+        let mut buffer = String::new();
+        File::open(self.channel_2_path)?.read_to_string(&mut buffer)?;
+        Ok(buffer.trim().parse::<u16>().unwrap())
+    }
 }
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
-use std::io::Result;
+use crate::adc::{Adc, AdcDevice};
 
 mod adc;
+fn main() {
+    let mut adc = Adc {
+        channel_1_path: "/sys/bus/iio/devices/iio:device0/in_voltage0_raw",
+        channel_2_path: "/sys/bus/iio/devices/iio:device0/in_voltage1_raw",
+    };
+    let channel1 = adc.read_channel1();
+    let channel2 = adc.read_channel2();
+    println!("Channel 1: {:?}", channel1);
+    println!("Channel 2: {:?}", channel2);
 
-fn main() -> Result<()> {
-    let mut adc = adc::Adc::new()?;
-    let voltage0 = adc.read_voltage0()?;
-    let voltage1 = adc.read_voltage1()?;
-    println!("Voltage 0: {}", voltage0);
-    println!("Voltage 1: {}", voltage1);
-    let sampling_frequency = adc.read_sampling_frequency()?;
-    println!("Sampling Frequency: {}", sampling_frequency);
-    adc.set_sampling_frequency(adc::SamplingFrequency::F16)?;
-    println!("Sampling frequency set to {}",adc.read_sampling_frequency()?);
-    println!("updated Sampling Frequency: {}", adc.read_sampling_frequency()?);
-    Ok(())
+    //wait for 5 seconds 
+    std::thread::sleep(std::time::Duration::from_secs(5));
+
+    //read again
+    let channel1 = adc.read_channel1();
+    let channel2 = adc.read_channel2();
+    println!("Channel 1: {:?}", channel1);
+    println!("Channel 2: {:?}", channel2);
 }
